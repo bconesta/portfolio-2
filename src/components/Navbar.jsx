a59fe4cd1e26a476e2fe1fe7e8480ec84664b222
Bruno Conesta <bconesta@etrr.edu.ar>
2023-01-21 01:43:20 -0300

    Navbar started

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
+import Bruno from '../img/bruno.jpg'
+
 export default function Navbar(props) {
   return (
     <div className={props.className}>
-      <a href="../files/resume.pdf" download="resume.pdf">Download</a>
+      <div className='container'>
+        <div className='foto-container'>
+          <img src={Bruno} alt="brunoconesta" />
+        </div>
+        <ul>
+          <li><a href="" className='selected'>BRUNO</a></li>
+          <li><a href="">ABOUT</a></li>
+          <li><a href="">PROJECTS</a></li>
+          <li><a href="">EDUCATION</a></li>
+          <li><a href="">CONTACT</a></li>
+        </ul>
+      </div>
     </div>
   )
 }
