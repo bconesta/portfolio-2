b53beb88c0b316ebeca729abbb743d739e6e926e
Bruno Conesta <bconesta@etrr.edu.ar>
2023-01-22 01:02:20 -0300

    Pages created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import { useEffect } from 'react';
 import './App.scss';
 import Navbar from './components/Navbar';
+import Bruno from './sections/Bruno';
+import About from './sections/About';
+import Projects from './sections/Projects';
+import Education from './sections/Education';
+import Contact from './sections/Contact';
 
 export default function App() {
 
@@ -19,19 +24,19 @@ export default function App() {
     <div className="App">
       <Navbar className="navbar"/>
       <div className="page initial" id="bruno">
-        <h1>HOLA</h1>
+        <Bruno />
       </div>
       <div className="page" id="about">
-        
+        <About /> 
       </div>
       <div className="page" id="projects">
-        
+        <Projects />
       </div>
       <div className="page" id="education">
-        
+        <Education />
       </div>
       <div className="page" id="contact">
-        
+        <Contact />
       </div>
     </div>
   );
---
+++ src/sections/Bruno.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Bruno() {
+  return (
+    <h1>Bruno</h1>
+  )
+}
